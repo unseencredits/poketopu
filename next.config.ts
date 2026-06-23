6baa49e0da6297d341c57ce611beeef505996f5b
Batuhan <batuhanaltunayy@gmail.com>
2026-06-24 00:16:42 +0300

    fix: images.scrydex.com remotePatterns'a eklendi

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,11 @@ const nextConfig: NextConfig = {
         hostname: 'eskipzstgoexqjgcxskl.supabase.co',
         pathname: '/storage/v1/object/public/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.scrydex.com',
+        pathname: '/**',
+      },
     ],
   },
 };
